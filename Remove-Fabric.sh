63f5642a4910e3e7d25afba3fce94989ed7a9737
Sudhishna <ssendhil@juniper.net>
2018-05-02 10:05:38 -0700

    Update Remove-Fabric.sh

--- Remove-Fabric.sh
+++ Remove-Fabric.sh
@@ -5,7 +5,7 @@
 
 printf  "Removing configuration begins.\r"
 
-ansible-playbook -i all.inv 01-Interfaces/remove-config.yml
+ansible-playbook -i all.inv 01-overlay/remove-config.yml
 sleep 5 
 
 printf "\n\nThe configurations were successfully removed.\n"
